8f8f01b6626a4afb181db63ce54c43ae471b94ed
DrMikeSh <81260128+DrMikeSh@users.noreply.github.com>
2025-02-08 23:12:18 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,8 +7,15 @@ def main_topics(text, client):
             "content": '''
             You will receive a user-provided text describing the desired code.
             Your task is to analyze the request and determine the most relevant programming modules or topics needed to fulfill it.
-            Stick to Streamlit modules only!
             Dont list general topics only topics specific to the user request. 
+            Examples: 
+            User: how to create a text input in streanlit?
+            Return: "text input"
+
+            User: how to build two text inputs one next to the other in streamlit?
+            Return: "text input; layout arrangement"
+
+            Stick to Streamlit modules only!
             Return a list of up to 10 of the most important modules or topics, separated by semicolons. 
             Important: return only the list of modules or topics, Noting else!
             '''
